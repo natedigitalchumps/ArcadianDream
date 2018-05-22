4090d2c85265812bd467d2280ae78c4873aa4758
sirerr <sirerr@gmail.com>
2018-05-22 09:09:08 -0400

    getting stuff started more

---
+++ ASSETS/UNITY/ArcadianV4/Assets/Scripts/ClawMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawMovement : MonoBehaviour {
+
+    Vector2 startPos;
+    float ParentSize;
+    private void Awake()
+    {
+        startPos.x = transform.localPosition.x;
+        startPos.y = transform.localPosition.z;
+
+        ParentSize = transform.parent.localScale.x;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float hor;
+        float ver;
+
+
+	}
+}
--- ASSETS/UNITY/ArcadianV4/Assets/Scripts/GameManager.cs
+++ ASSETS/UNITY/ArcadianV4/Assets/Scripts/GameManager.cs
@@ -26,8 +26,7 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("Hello Game Manager");
-
+ 
 	}
 	
 	// Update is called once per frame
