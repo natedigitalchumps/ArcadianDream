d07ea648d3eea61caa0d0e8274bc4d37e76e4dd9
sirerr <sirerr@gmail.com>
2018-05-27 19:11:41 -0400

    working on getting stuff to work

---
+++ Assets/Scripts/ClawPowers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawPowers : MonoBehaviour {
+
+    public enum grabstate {empty,full};
+    public grabstate clawGrabState;
+    float Grabvalue;
+    public float GrabValueLimit = .6f;
+    GameObject GrabbedOjbect;
+    GrabberControl grabberRef;
+    // Use this for initialization
+    private void Awake()
+    {
+        grabberRef = GetComponent<GrabberControl>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+
+
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+
+    }
+    
+}
---
+++ Assets/Scripts/GrabberControl.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabberControl : MonoBehaviour {
+
+    public enum CraneState { Top, Moving, Floor };
+    public CraneState CraneLocation;
+
+    public enum grabstate { empty, full };
+    public grabstate clawGrabState;
+
+    //grabbing a object
+    float Grabvalue;
+    public float GrabValueLimit = .6f;
+    GameObject GrabbedOjbect;
+
+
+    //rigid body
+    public Rigidbody rbody;
+    //come back up
+    bool doUP = false;
+    //final point
+    public Transform point;
+    
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.name == "floor" || col.collider.tag == "toy")
+        {
+            CraneLocation = CraneState.Floor;
+
+        }
+
+        if (col.collider.tag == "toy" && clawGrabState == grabstate.empty)
+        {
+            Grabvalue = Random.value;
+            print(Grabvalue);
+            if (Grabvalue > GrabValueLimit && clawGrabState == grabstate.empty)
+            {
+                clawGrabState = grabstate.full;
+                GrabbedOjbect = col.gameObject;
+                GrabbedOjbect.transform.parent = transform;
+                Rigidbody ToyRBody = GrabbedOjbect.GetComponent<Rigidbody>();
+                ToyRBody.isKinematic = true;
+                rbody.isKinematic = true;
+            }
+        }
+
+
+    }
+
+
+    private void Awake()
+    {
+ 
+        rbody = GetComponent<Rigidbody>();
+  
+    }
+
+    private void Update()
+    {
+
+
+        if (Input.GetMouseButtonDown(0) && clawGrabState == grabstate.full)
+        {
+            GrabbedOjbect.transform.parent = null;
+            Rigidbody rbody = GrabbedOjbect.GetComponent<Rigidbody>();
+            rbody.isKinematic = false;
+            clawGrabState = grabstate.empty;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && CraneLocation == CraneState.Top)
+        {
+
+            rbody.useGravity = true;
+            CraneLocation = CraneState.Moving;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && CraneLocation == CraneState.Floor)
+        {
+            rbody.useGravity = false;
+            CraneLocation = CraneState.Moving;
+            doUP = true;
+        }
+
+        if (doUP)
+        {
+           
+            float dis = Vector3.Distance(transform.position, point.position);
+            if(dis<.001)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, point.position, 0);
+            }else
+            {
+                doUP = false;
+                CraneLocation = CraneState.Top;
+            }
+        }
+
+    }
+}
